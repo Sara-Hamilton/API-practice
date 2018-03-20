aec30580eeeb990748c4e3af9b0baf7fcf885ac0
Hannah Lee and Sara Hamilton <hannah@>
2018-03-20 10:00:58 -0700

    change from ajax to XMLHttpRequest and add .env file.

--- weather-interface.js
+++ weather-interface.js
@@ -1,20 +1,47 @@
+// AJAX request
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     let city = $('#location').val();
+//     $('#location').val("");
+//     $.ajax({
+//       url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=2668c07da7e23016b049c1d4656f0af8`,
+//       type: 'GET',
+//       data: {
+//         format: 'json'
+//       },
+//       success: function(response) {
+//         $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//         $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+//       },
+//       error: function() {
+//         $('.errors').text("There was an error processing your request. Please try again.")
+//       }
+//     });
+//   });
+// });
+
+// XMLHttpRequest
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     $('#location').val("");
-    $.ajax({
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=2668c07da7e23016b049c1d4656f0af8`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-      },
-      error: function() {
-        $('.errors').text("There was an error processing your request. Please try again.")
+
+    let request = new XMLHttpRequest();
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2668c07da7e23016b049c1d4656f0af8`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        let response = JSON.parse(this.responseText);
+        getElements(response);
       }
-    });
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    getElements = function(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
   });
 });
